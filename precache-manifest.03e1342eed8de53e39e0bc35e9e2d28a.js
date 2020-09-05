0475e044578ca91aece75f0c4d64addf0415d346
Abhijeet Banerjee <abhijeet.banerjee@hexagonglobal.in>
2020-09-05 21:15:13 +0530

    Updates

---
+++ precache-manifest.03e1342eed8de53e39e0bc35e9e2d28a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cb4e31b119ce2fabd48d842d17df01",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a0cf7971f546710bf861",
+    "url": "/monsters-rolodex/static/css/main.6a46a216.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0cf7971f546710bf861",
+    "url": "/monsters-rolodex/static/js/main.d9b5eecd.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
